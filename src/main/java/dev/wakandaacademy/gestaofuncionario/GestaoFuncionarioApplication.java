5ea1e616ac924dbc2c1f25b435678940ad85e5b5
Marcio <augusto_sena@hotmail.com>
2024-04-02 22:36:46 -0300

    init commit

---
+++ src/main/java/dev/wakandaacademy/gestaofuncionario/GestaoFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.gestaofuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacademy/gestaofuncionario/GestaoFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.gestaofuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
