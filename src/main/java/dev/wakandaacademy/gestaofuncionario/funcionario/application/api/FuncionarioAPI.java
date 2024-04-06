bedc3bc19c8c7a4d3828532f851e5b1cfe0f90cd
Marcio <marcio.augusto.o.s@gmail.com>
2024-04-06 15:17:54 -0300

    implementa service

--- src/main/java/dev/wakandaacademy/gestaofuncionario/funcionario/application/api/FuncionarioAPI.java
+++ src/main/java/dev/wakandaacademy/gestaofuncionario/funcionario/application/api/FuncionarioAPI.java
@@ -3,6 +3,7 @@ package dev.wakandaacademy.gestaofuncionario.funcionario.application.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/v1/funcionarios")
@@ -10,4 +11,8 @@ public interface FuncionarioAPI {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     FuncionarioCriadoResponse postNovoFuncionario(@RequestBody @Valid FuncionarioNovoRequest funcionarioNovo);
+
+    @GetMapping("/{idFuncionario}")
+    @ResponseStatus(code = HttpStatus.OK)
+    FuncionarioCriadoResponse buscaFuncionarioPorId(@PathVariable UUID idFuncionario);
 }
